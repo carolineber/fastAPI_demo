6af5785cfe06b4d44c1eaa2125b656547e8ae029
carolineber <carolinebernardosilva@gmail.com>
2022-11-06 21:20:14 -0300

    :passport_control: Adding Auth

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime, time
+from typing import Optional
     
 class PostBase(BaseModel):
     title:str
@@ -25,4 +26,14 @@ class UserOut(BaseModel):
     created_at: datetime 
     class Config: # convert in dict
         orm_mode = True
-    
+
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
